c40d0503cb3dd504c19bff06599914fe7280bc8e
tengwang <tengwang@cori.nersc.gov>
2021-10-09 16:16:39 -0700

    Accelerate slurm dataframe generation

--- iominer/gen_pandas_for_slurm.py
+++ iominer/gen_pandas_for_slurm.py
@@ -41,101 +41,112 @@ import string
 import re
 #from slurm_plot import *
 
-def join_pd(job_tot_df, job_day_df_lst):
-    for job_day_df in job_day_df_lst:
-#        job_day_df.info()
-        for (row_label, row_series) in job_day_df.iterrows():
-            for column,value in row_series.items():
-                job_tot_df.loc[row_label, column] = value
+def join_pd(job_tot_df, job_day_df):
+    for (row_label, row_series) in job_day_df.iterrows():
+#        print("row_label:%s, item count:%d\n"%(row_label, len(job_day_df.iterrows())))
+        for column,value in row_series.items():
+            job_tot_df.loc[row_label, column] = value
+        print("finished row_label:%s\n"%row_label)
 
 def convert_to_df(f, dst_dir, start_ts, end_ts):
     
-    filename_w_date = "%s/slurm_df_%d_%d"%(dst_dir, start_ts, end_ts) 
-    try:
-        tot_fd = open(filename_w_date, 'wb')
-    except IOError as e:
-        print("Fail to open file %s:%s"%(filename_w_date, e))
-        return -1
-  
     text = f.readline()
     header_lines = text.split("|")
     useful_metas = ["JobID", "User", "JobName", "Start", "End", "Elapsed", "State", "AllocNodes", "NTasks", "AllocCPUS", "ReqCPUS"]
 
+    slurm_table = {}
     slurm_df = pd.DataFrame()
 
     glb_cursor = 0
     while True:
         text = f.readline()
-#        print("line is %s, glb_cursor is %d\n"%(text, glb_cursor))
+        print("line is %s, glb_cursor is %d\n"%(text, glb_cursor))
+#        if glb_cursor == 5:
+#            break
         if not text:
+            print("breaking\n")
+            sys.stdout.flush()
             break
         if glb_cursor >= 0:
             cursor = 0
             tmp_dict = {}
             record_lines = text.split("|")
             job_name = record_lines[0].split('.')[0].split('_')[0]
-#            print("job_name is %s\n"%job_name)
-            if job_name in slurm_df.index:
-                cur_cnt = int(slurm_df.loc[job_name, "count"])
-                slurm_df.loc[job_name, "count"] = cur_cnt + 1
+#            print("job_name is %s, record line length is %d\n"%(job_name, len(record_lines)))
+            if job_name in slurm_table:
+                cur_cnt = int(slurm_table[job_name]["count"])
+                slurm_table[job_name]["count"] = cur_cnt + 1
                 job_name = job_name + "." + str(cur_cnt + 1)
-
-#               print "job_name is %s, end:%s, cursor:%d, user:%s, app:%s\n"%(tmp_dict["JobID"], tmp_dict["End"], glb_cursor, tmp_dict["User"], tmp_dict["JobName"]) 
+                slurm_table[job_name] = {}
+#                print("job_name1:%s\n"%job_name)
+#                print "job_name1 is %s, end:%s, cursor:%d, user:%s, app:%s\n"%(tmp_dict["JobID"], tmp_dict["End"], glb_cursor, tmp_dict["User"], tmp_dict["JobName"]) 
             else:
-                slurm_df.loc[job_name, "count"] = 1
+                slurm_table[job_name] = {}
+                slurm_table[job_name]["count"] = 1
                
-#            print("job name:%s\n"%job_name)
+#                print("job name2:%s\n"%job_name)
+            print(record_lines)
             for tmp_str in record_lines:
                 if header_lines[cursor] not in useful_metas:
                     cursor += 1
                     continue
                 if "JobID" == header_lines[cursor]:
-                    if "COMPLETED" not in tmp_str:
+                    if "COMPLETED" not in record_lines:
                         cursor += 1
                         continue
         
 #                    if "batch" in tmp_str or "extern" in tmp_str:
 #                        cursor += 1
 #                        continue
-                
-                slurm_df.loc[job_name, header_lines[cursor]] = tmp_str
+               
+#                print("setting jobname:%s, headerlines:%s, value:%s\n"%(job_name, header_lines[cursor], tmp_str))
+                slurm_table[job_name][header_lines[cursor]] = tmp_str
                 
                 if header_lines[cursor] == "NTasks":
-                    str_ntasks = slurm_df.loc[job_name, header_lines[cursor]]
+                    str_ntasks = slurm_table[job_name][header_lines[cursor]]
                     if tmp_str != "":
                         idx = str_ntasks.find('K')
                         if idx != -1: 
                             ntasks = float(str_ntasks[0:idx]) * 1000
                             ntasks = int(ntasks)
-                            slurm_df.loc[job_name, header_lines[cursor]] = ntasks
+                            slurm_table[job_name][header_lines[cursor]] = ntasks
                 if header_lines[cursor] == "Elapsed":
-                    time_str = slurm_df.loc[job_name, header_lines[cursor]]
-                    slurm_df.loc[job_name, header_lines[cursor]] = get_sec(time_str)
+                    time_str = slurm_table[job_name][header_lines[cursor]]
+                    slurm_table[job_name][header_lines[cursor]] = get_sec(time_str)
                 if header_lines[cursor] == "AllocCPUS":
-                    str_alloc_cpus = slurm_df.loc[job_name, header_lines[cursor]] 
+                    str_alloc_cpus = slurm_table[job_name][header_lines[cursor]] 
                     idx = str_alloc_cpus.find('K')
                     if idx != -1:
                         alloc_cpus = float(str_alloc_cpus[0:idx]) * 1000
-                        slurm_df.loc[job_name, header_lines[cursor]]  = int(alloc_cpus)
+                        slurm_table[job_name][header_lines[cursor]]  = int(alloc_cpus)
                 if header_lines[cursor] == "AllocNodes":
-                    str_alloc_nodes = slurm_df.loc[job_name, header_lines[cursor]] 
+                    str_alloc_nodes = slurm_table[job_name][header_lines[cursor]] 
                     idx = str_alloc_nodes.find('K')
                     if idx != -1:
                         alloc_nodes = float(str_alloc_nodes[0:idx]) * 1000
-                        slurm_df.loc[job_name, header_lines[cursor]]  = int(alloc_nodes)
+                        slurm_table[job_name][header_lines[cursor]]  = int(alloc_nodes)
 #                print("%s:%s"%(header_lines[cursor], str(slurm_df.loc[job_name][header_lines[cursor]])))
                         
                 cursor = cursor + 1
+#            print("after, job_name is %s, record line length is %d\n"%(job_name, len(record_lines)))
 #                print "job_name is %s, end:%s, cursor:%d, user:%s, app:%s\n"%(tmp_dict["JobID"], tmp_dict["End"], glb_cursor, tmp_dict["User"], tmp_dict["JobName"]) 
         glb_cursor = glb_cursor + 1
+        sys.stdout.flush()
+   
+    print("converting to dataframe...\n")
+    sys.stdout.flush()
+    slurm_df = pd.DataFrame.from_dict(slurm_table, orient = 'index')
+    print("after converting to dataframe...\n")
+    sys.stdout.flush()
 #        print("\n")
-    pickle.dump(slurm_df, tot_fd, -1)
-    tot_fd.close()
+#    pickle.dump(slurm_df, tot_fd, -1)
+#    tot_fd.close()
+    return slurm_df
 
 def format_slurm_files(src_slurm_dir, dst_slurm_dir, start_ts, end_ts):
     
     fmt_slurm_files = src_slurm_dir + "slurm_*.log"
-    print("fmt_slurm_files is %s\n"%fmt_slurm_files)
+#    print("fmt_slurm_files is %s\n"%fmt_slurm_files)
     file_list = glob.glob(fmt_slurm_files)
     qualified_files = []
     
@@ -143,10 +154,9 @@ def format_slurm_files(src_slurm_dir, dst_slurm_dir, start_ts, end_ts):
     
     FNAME_PATTERN = 'slurm_([0-9]+)_([0-9]+).log'
     
-    fname_pat = re.compile(FNAME_PATTERN)
-    
+    fname_pat = re.compile(FNAME_PATTERN)   
     for cur_file in file_list:
-        print("cur file is %s\n"%cur_file)
+#        print("#####cur file is %s, start_ts is %d, end_ts is %d\n"%(cur_file, start_ts, end_ts))
         fname_match = fname_pat.match(os.path.basename(cur_file))
         
         if not fname_match:
@@ -161,14 +171,29 @@ def format_slurm_files(src_slurm_dir, dst_slurm_dir, start_ts, end_ts):
     for cur_file in qualified_files:
         with open(cur_file) as fd:
             df = convert_to_df(fd, dst_slurm_dir, start_ts, end_ts )
-            join_pd(job_tot_df, df)
-#    
-#
-# 
-#    tmp_str = "%sformat_slurm%s_%s.log"%(format_slurm_root, start_ts, end_ts)
-#    save_fd = open(tmp_str, 'wb')
-#    pickle.dump(job_tot_df, save_fd, -1)
-#    save_fd.close()
+#            print(df)
+#            join_pd(job_tot_df, df)
+#            print(df)
+            print(df[["JobID"]])
+            if job_tot_df.empty:
+                job_tot_df = job_tot_df.append(df, ignore_index = True)
+            else:
+                job_tot_df = job_tot_df.append(df)
+#            print(job_tot_df[["JobID"]])
+        print("finished joining %s\n"%cur_file)
+        sys.stdout.flush()
+
+
+    
+
+    tmp_str = "%s/slurm_df_%s_%s.log"%(dst_slurm_dir, start_ts, end_ts)
+    print("flushing to file:%s"%tmp_str)
+    sys.stdout.flush()
+    save_fd = open(tmp_str, 'wb')
+    pickle.dump(job_tot_df, save_fd, -1)
+    save_fd.close()
+    print("finished flushing to file:%s"%tmp_str)
+    sys.stdout.flush()
 
 
 def get_sec(time_str):
